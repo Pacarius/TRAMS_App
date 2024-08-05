8d0823ec1fc34601a68dea167b1aa8da1fe8b93f
Pacarius <pcpgsk@gmail.com>
2024-08-05 17:03:59 +0800

    I don't know how to use github

--- components/Buttons/SampleButton.tsx
+++ components/Buttons/SampleButton.tsx
@@ -4,7 +4,7 @@ const SampleButton = () => {
     return(<StandardButton
         icon={require('../../assets/images/react-logo.png')} 
         text='Ech.' 
-        action={() => {console.log('Balls')}}
+        action={() => {console.log('Ballers')}}
       ></StandardButton>
     )
 }
