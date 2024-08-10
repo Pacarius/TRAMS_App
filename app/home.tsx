934c3c2f80e234685ec559480d2bf95450ac055d
Pacarius <pcpgsk@gmail.com>
2024-08-10 18:23:02 +0800

    Made index redirectory

---
+++ app/home.tsx
@@ -0,0 +1,19 @@
+import SampleCategory from "@/components/Categories/SampleCategory";
+import { Text, ScrollView, View } from "react-native";
+import { styles } from "@/constants/styles";
+import TopBar from "@/components/TopBar";
+export default function Home() {
+  return (
+    <View style={{flex:1}}>
+      <TopBar></TopBar>
+      <ScrollView style={styles.index}
+      contentContainerStyle={styles.container}>
+      <SampleCategory log='1'></SampleCategory>
+      <SampleCategory log='2'></SampleCategory>
+      <SampleCategory log='3'></SampleCategory>
+      <SampleCategory log='4'></SampleCategory>
+      <SampleCategory log='5'></SampleCategory>
+      </ScrollView>
+    </View>
+  );
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,4 @@
-import SampleCategory from "@/components/Categories/SampleCategory";
-import { Text, ScrollView, View } from "react-native";
-import { styles } from "@/constants/styles";
-import TopBar from "@/components/TopBar";
+import { Redirect } from "expo-router";
 export default function Index() {
-  return (
-    <View style={{flex:1}}>
-      <TopBar></TopBar>
-      <ScrollView style={styles.index}
-      contentContainerStyle={styles.container}>
-      <SampleCategory log='1'></SampleCategory>
-      <SampleCategory log='2'></SampleCategory>
-      <SampleCategory log='3'></SampleCategory>
-      <SampleCategory log='4'></SampleCategory>
-      <SampleCategory log='5'></SampleCategory>
-      </ScrollView>
-    </View>
-  );
+  return <Redirect href="home"></Redirect>;
 }
