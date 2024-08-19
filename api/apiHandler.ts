579db62bfc2feacece08fc6bea1b9ecb4371970d
Pacarius <pcpgsk@gmail.com>
2024-08-19 15:01:44 +0800

    Added placeholder api.

--- api/apiHandler.ts
+++ api/apiHandler.ts
@@ -1,1 +1,18 @@
-const ip : string = '';
+const ip: string = '';
+class ApiHandler {
+    public getVehicleInfo(){
+        return new VehicleInformation("TEMP", "AB1234", VehicleType.CAR);
+    }
+}
+class VehicleInformation {
+    constructor(
+        public readonly ownerName: string, 
+        public readonly vehicleID: string, 
+        public readonly vehicleType: VehicleType){}
+}
+enum VehicleType {
+    UNDEFINED = 0,
+    CAR,
+    MOTORCYCLE,
+    
+}
